057d1f02f581ad3c609d4f21196c2f6c9148e378
Jasterrr <ivan.bernatovic.93@gmail.com>
2015-03-26 15:51:40 +0100

    Changed database connection info
    Removed my personal username and password for MySQL database

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'lovebook',
-			'username'  => 'root',
-			'password'  => 'root',
+			'database'  => 'yourDatabaseName',
+			'username'  => '',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
